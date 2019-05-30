54d97fcc7cdcdb013a2e0dd0f90e34334eb22166
qiuz <zhu_anly@163.com>
2019-05-30 18:04:10 +0800

    image preview hero animation

--- lib/component/custom-route/custom-route.dart
+++ lib/component/custom-route/custom-route.dart
@@ -18,39 +18,39 @@ class CustomRoute extends PageRouteBuilder {
               Animation<double> animation,
               Animation<double> secondaryAnimation,
               Widget child) {
-            //淡出过渡路由
-          // return FadeTransition(
-          //     opacity: Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
-          //         parent: animation, curve: Curves.fastOutSlowIn)),
-          //     child: child,
-          //     );
+            // 淡出过渡路由
+            // return FadeTransition(
+            //   opacity: Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+            //       parent: animation, curve: Curves.fastOutSlowIn)),
+            //   child: child,
+            // );
 
-            //比例转换路由
-         return ScaleTransition(
-           scale: Tween(begin: 0.2, end: 1.0).animate(CurvedAnimation(
-               parent: animation, curve: Curves.fastLinearToSlowEaseIn)),
-           child: child,
-           );
+            // 比例转换路由
+            return ScaleTransition(
+              scale: Tween(begin: 0.2, end: 1.0).animate(CurvedAnimation(
+                  parent: animation, curve: Curves.fastLinearToSlowEaseIn)),
+              child: child,
+            );
 
-            //旋转+比例转换路由
-//            return RotationTransition(
-//              turns: Tween(begin: -1.0, end: 1.0).animate(CurvedAnimation(
-//                  parent: animation, curve: Curves.fastOutSlowIn)),
-//              child: ScaleTransition(
-//                scale: Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
-//                    parent: animation, curve: Curves.fastOutSlowIn)),
-//                child: child,
-//              ),
-//            );
+            // 旋转+比例转换路由
+            // return RotationTransition(
+            //   turns: Tween(begin: -1.0, end: 1.0).animate(CurvedAnimation(
+            //       parent: animation, curve: Curves.fastOutSlowIn)),
+            //   child: ScaleTransition(
+            //     scale: Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+            //         parent: animation, curve: Curves.fastOutSlowIn)),
+            //     child: child,
+            //   ),
+            // );
 
-            //幻灯片路由
-          //  return SlideTransition(
-          //    position:
-          //        Tween<Offset>(begin: Offset(0.0, -1.0), end: Offset(0.0, 0.0))
-          //            .animate(CurvedAnimation(
-          //                parent: animation, curve: Curves.fastOutSlowIn)),
-          //    child: child,
-          //  );
+            // 幻灯片路由
+            // return SlideTransition(
+            //   position:
+            //       Tween<Offset>(begin: Offset(0.0, -1.0), end: Offset(0.0, 0.0))
+            //           .animate(CurvedAnimation(
+            //               parent: animation, curve: Curves.fastOutSlowIn)),
+            //   child: child,
+            // );
           },
         );
 }
